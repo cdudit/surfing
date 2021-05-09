b013c32aae2616c38e41cfc0b445de2bbd085cea
Clement Dudit <clement.dudit@etudiant.univ-lr.fr>
2021-05-09 23:05:24 +0200

    spot meilleur disposition

--- Surfing/controllers/SpotsViewController.swift
+++ Surfing/controllers/SpotsViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SpotsViewController: UIViewController {
 
+    @IBOutlet weak var stack: UIStackView!
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var image: UIImageView!
     var spots: [Spot] = []
@@ -42,15 +43,4 @@ class SpotsViewController: UIViewController {
         }
         setSpot(spot: spots[index])
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
